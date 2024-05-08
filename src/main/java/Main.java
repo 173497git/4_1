8c22fabc3490ddb9e4c5c2a7aedf23d0b8f30bfe
173497git <161622376+173497git@users.noreply.github.com>
2024-05-08 15:08:21 +0000

    4_1
    4_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,21 +7,49 @@ Kod bazowy programu Commit4_0:
 • Klasa Student reprezentuje pojedynczego studenta (Imię, Wiek).
 */
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-class Main {
-  public static void main(String[] args) {
-    try {
-      Service s = new Service();
-      s.addStudent(new Student("Krzysztof", 20));
-      s.addStudent(new Student("Janusz", 40));
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Service s = new Service();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            boolean addingStudents = true;
 
-      var students = s.getStudents();
-      for(Student current : students) {
-        System.out.println(current.ToString());
-      }
-    } catch (IOException e) {
+            while (addingStudents) {
+                System.out.println("Co chcesz zrobić?");
+                System.out.println("1. Dodaj nowego studenta");
+                System.out.println("2. Zakończ dodawanie studentów");
 
+                String choice = reader.readLine();
+
+                switch (choice) {
+                    case "1":
+                        System.out.println("Podaj imię nowego studenta:");
+                        String name = reader.readLine();
+                        System.out.println("Podaj wiek nowego studenta:");
+                        int age = Integer.parseInt(reader.readLine());
+                        s.addStudent(new Student(name, age));
+                        System.out.println("Dodano nowego studenta.");
+                        break;
+                    case "2":
+                        addingStudents = false;
+                        break;
+                    default:
+                        System.out.println("Niepoprawny wybór. Spróbuj ponownie.");
+                        break;
+                }
+            }
+
+            System.out.println("Lista studentów w bazie:");
+            for (Student student : s.getStudents()) {
+                System.out.println(student);
+            }
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd wejścia/wyjścia: " + e.getMessage());
+        }
     }
-  }
 }
+
